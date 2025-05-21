e9b6641b107b4ce89994a0d67164a06f0596554a
sandip_behera <sandipbehera321@gmail.com>
2025-05-21 13:03:09 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig 
 from src.mlproject.components.data_transformation import DataTransformationConfig
 from src.mlproject.components.data_transformation import DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainer , ModelTrainerConfig
 
 
 import sys
@@ -19,7 +20,14 @@ if __name__=="__main__":
 
         #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path , test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path , test_data_path)
+
+
+        ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
     except Exception as e:
         logging.error("Error occurred")
         raise CustomException(e,sys) 
--- template.py
+++ template.py
@@ -11,7 +11,7 @@ list_of_files=[
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
-    f"src/{project_name}/components/model_tranier.py",
+    f"src/{project_name}/components/model_trainer.py",
     f"src/{project_name}/components/model_monitering.py",
     f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipeline.py",
